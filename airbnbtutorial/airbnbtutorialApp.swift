4e7e815312dfd940ea8fc0f795f196e6499d9214
Guillermo Reyes <reyesguillermo54@yahoo.com>
2025-03-09 11:09:06 -0500

    Initial Commit

---
+++ airbnbtutorial/airbnbtutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbtutorialApp.swift
+//  airbnbtutorial
+//
+//  Created by Guillermo Reyes on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct airbnbtutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
